84dc4c354ceb746c0241e61d362f5b41069dad64
James Ahlm <14782828428@qq.com>
2016-11-14 10:12:49 +0800

    Added a css file, might have margins

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,21 +1,51 @@
-var height = 960;
-var width = 620;
+//Adding Margins and fixing formatting
+
+var margin = {
+top: 5,
+right: 0,
+bottom: 90,
+left: 100
+}
+
+var height = 1200 - margin.left - margin.right;
+var width = 550 - margin.top - margin.bottom;
 
 d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function(data){
 data = data.data
 
-d3.select('svg')
-.attr("height", height)
-.attr("width", width)
 
-var gdpMax = d3.max(data, function(d) {return d[1]})
-var gdpMin = d3.min(data, function(d) {return d[1]})
+var gdpData = data.map(function(d){return d[1]})
+var yearData = data.map(function(d){return d[0]})
+
+var gdpMax = d3.max(gdpData)
+var gdpMin = d3.min(gdpData)
+var yearMax = d3.max(yearData)
+var yearMin = d3.min(yearData)
+
+
+
 var yScale = d3.scaleLinear()
 .domain([0, gdpMax])
 .range([0,960]);
 
 var xScaleValue = width/(data.length)
 var barWidth = xScaleValue * .8
+
+
+//Put a g inside an svg in order to get margins
+d3.select('svg')
+.attr("height", height + margin.top + margin.bottom)
+.attr("width", width + margin.left + margin.right)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+.attr("id", "g-cont")
+
+
+//Add a tooltip?
+
+//Add a y yourself?
+
+//Generate Graph with elements
 
 d3.select("svg")
 .selectAll("rect")
